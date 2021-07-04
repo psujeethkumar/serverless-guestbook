7d9984b1e1cfcfaf223ad6936b79a2bf409d5221
psujeethkumar <psujeethkumar@gmail.com>
2021-07-04 23:28:59 +0200

    Added the correct apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2cb1b7b5.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
